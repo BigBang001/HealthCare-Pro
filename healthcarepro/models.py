79239fd144b4c3e95bba0eec34f8b68d388e56a4
BigBang001 <119509166+BigBang001@users.noreply.github.com>
2025-06-06 17:17:25 +0530

    Update models.py

--- healthcarepro/models.py
+++ healthcarepro/models.py
@@ -14,7 +14,7 @@ class Patient(models.Model):
 
     def __str__(self):
         return self.name
-
+        
 class Doctor(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
